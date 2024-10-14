4d5a05a585a143faeda4b2040a73b2badff09451
JohnS300 <johnsofokleous@gmail.com>
2024-10-15 00:20:43 +0300

    Float's Explicit casting with int

--- firstapp.cs
+++ firstapp.cs
@@ -11,9 +11,13 @@ namespace FirstApp{
             //Console.WriteLine("First Application");
 
             int i , x, z, y;
+            //float declaration requires F
+            float f1= 15.54F;
 
-            i = 5; x = 10 ; z = y = 25;
+            i = 5; x = 10 ; z = y = 25; 
             int sum = i + x + z + y;
+            //Explicit casting with int
+            sum += (int)f1; 
             Console.WriteLine($"Sum = {sum} ");
 
         }
