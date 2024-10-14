0b52843d682fd40ce6e8afa394f03e4a33409446
JohnS300 <johnsofokleous@gmail.com>
2024-10-15 00:05:53 +0300

    successful run with variables and string interpolation

--- firstapp.cs
+++ firstapp.cs
@@ -1,14 +1,21 @@
 using System;
-
-namespace FirstApp{
-    class FirstApp{
-        static void Main(string[] args){
-            Console.WriteLine("First Application");
-        }
-    }
-}
 //testing functionalities of C#
 
 /*
 multiline comments similar to java
 */
+
+namespace FirstApp{
+    class FirstApp{
+        static void Main(string[] args){
+            //Console.WriteLine("First Application");
+
+            int i , x, z, y;
+
+            i = 5; x = 10 ; z =20 ; y = 25;
+            int sum = i + x + z + y;
+            Console.WriteLine($"Sum = {sum} ");
+
+        }
+    }
+}
