7ced0d8257771f6f07cd50dd28ade3401f8c8c06
JohnS300 <johnsofokleous@gmail.com>
2024-10-14 23:54:36 +0300

    renaming

---
+++ firstapp.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace FirstApp{
+    class FirstApp{
+        static void Main(string[] args){
+            Console.WriteLine("First Application");
+        }
+    }
+}
+//is this main?
