bc8d1a609cfd85e4143f4ed78ca72239472a7367
JohnS300 <johnsofokleous@gmail.com>
2024-10-20 20:04:42 +0300

    Creating a datetime object to get the currentt day of the week.

--- firstapp.cs
+++ firstapp.cs
@@ -25,17 +25,28 @@ namespace FirstApp{
             //User Input with explicit conversion to Int using Conver.To method
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"So your age is : {age} , correct Y/N?");
+
             //Use a Ternary operator
             bool correct = Console.ReadLine().ToUpper() == "Y"? true : false ;
             //the below is the faster aproach
             //bool correct = Console.ReadLine().ToUpper() == "Y";
             Console.WriteLine(correct);
             
-
+            Console.WriteLine("Declare your current weight: ");
             int weight = Convert.ToInt32(Console.ReadLine());
             if (weight < 40 || weight > 90){
                 Console.WriteLine($"Your weight is {weight} and that means you need to go speak with a doctor.");
             };
+
+
+
+            //Creating a datetime object to get the currentt day of the week.
+            //DateTime currentDate = DateTime.Now;
+            //DayOfWeek currentDay = currentDate.DayOfWeek;
+
+            //Creating a datetime object to get the currentt day of the week.Shortened version.
+            DayOfWeek currentDay = DateTime.Now.DayOfWeek;
+            Console.WriteLine($"Today is: {currentDay}");
         }
     }
 }
